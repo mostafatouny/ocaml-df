48554e77d94866a341ed2bff8efc16160ebe73e9
Mostafa Touny <mostafatouny@protonmail.com>
2025-07-28 23:21:03 +0300

    Mon Jul 28 11:21:03 PM EEST 2025

--- arrayOps.ml
+++ arrayOps.ml
@@ -1,57 +1,25 @@
-(* Module signature for types that can be compared *)
-module type COMPARABLE = sig
-  type t
-  val equal : t -> t -> bool
-end
+module ArrayOps = struct
+    type 'a t = 'a array
+    type index = int
 
-(* Generic array operations signature *)
-module type ARRAY_OPS = sig
-  type 'a t
-  type index = int
-  
-  val make : int -> 'a -> 'a t
-  val length : 'a t -> int
-  val get : 'a t -> index -> 'a
-  val iteri: (int -> 'a -> unit) -> 'a array -> unit
-  val mapi : (index -> 'a -> 'b) -> 'a t -> 'b t
-end
+    let make = Array.make
+    let length = Array.length
+    let get = Array.get
 
-(* Standard array implementation *)
-module ArrayOps : ARRAY_OPS with type 'a t = 'a array = struct
-  type 'a t = 'a array
-  type index = int
+    let iteri = Array.iteri
+    let mapi = Array.mapi
 
-  let make = Array.make
-  let length = Array.length
-  let get = Array.get
+    let find_el_index x = Array.find_index ((==) x)
 
-  let iteri = Array.iteri
-  let mapi = Array.mapi
-end
+    let count el arr =
+        let counter = ref 0 in
+            iteri (fun _ x -> if (==) x el then incr counter) arr;
+        !counter
 
-(* Functor for creating search operations on arrays *)
-module MakeSearchable (A : ARRAY_OPS) (C : COMPARABLE) = struct
-  (* type t = C.t A.t *)
-  
-  let indexes arr ~value =
-    let indices = ref [] in
-    let add_if_match i x =
-      if C.equal x value then
-        indices := i :: !indices
-    in
-    A.iteri add_if_match arr;
-    List.rev !indices
-  
-  let find_first arr ~value =
-    let rec search i =
-      if i >= A.length arr then None
-      else if C.equal (A.get arr i) value then Some i
-      else search (i + 1)
-    in
-    search 0
-  
-  let count arr ~value =
-    let counter = ref 0 in
-    A.iteri (fun _ x -> if C.equal x value then incr counter) arr;
-    !counter
+    let indexes el arr =
+        let indices = ref [] in
+        let add_if_match i x =
+            if (==) x el then indices := i :: !indices
+        in iteri add_if_match arr;
+        List.rev !indices
 end
