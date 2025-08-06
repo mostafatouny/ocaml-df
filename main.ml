312d8d4a6c3e685b57bde7df6b04303b95335835
Mostafa Touny <mostafatouny@protonmail.com>
2025-08-06 15:28:46 +0300

    Wed Aug  6 03:28:46 PM EEST 2025

--- main.ml
+++ main.ml
@@ -5,19 +5,24 @@ let () =
     and ages = Column.of_list "town" ["Cairo"; "Greece"; "London"] in
 
     (* print names column *)
+    print_endline "print names column";
     Column.print names;
 
     (* create dataframe *)
     let df = Dataframe.create [names; ages] in
 
     (* print *)
+    print_endline "print dataframe";
     Dataframe.print df;
 
     (* add column then print *)
+    print_endline "add column adult";
     Column.of_list "adult" ["True"; "True"; "False"] |> Dataframe.addColumn df |> Dataframe.print;
 
     (* select column then print *)
+    print_endline "select column name";
     Dataframe.select df ["name"] |> Dataframe.print;
 
     (* Filter data *)
+    print_endline "filter dataframe";
     Dataframe.filter df (fun row -> List.hd row == "Alice") |> Dataframe.print
